41b387b04e09f23d87bb39bf06bf83f6800a2a29
Samuel Farrens <samuel.farrens@gmail.com>
2019-09-18 10:46:20 +0200

    updated

--- bobafett/info.py
+++ bobafett/info.py
@@ -7,7 +7,7 @@ This module provides some basic information about the package.
 """
 
 # Set the package release version
-version_info = (0, 0, 1)
+version_info = (0, 0, 3)
 __version__ = '.'.join(str(c) for c in version_info)
 
 # Set the package details
@@ -21,8 +21,8 @@ __requires__ = []  # Your package dependencies
 
 # Default package properties
 __license__ = 'MIT'
-__about__ = ('{} \n\n Author: {} \n Email: {} \n Year: {} \n {} \n\n'
-             ''.format(__name__, __author__, __email__, __year__,
-                       __long_description__))
+# __about__ = ('{} \n\n Author: {} \n Email: {} \n Year: {} \n {} \n\n'
+#              ''.format(__name__, __author__, __email__, __year__,
+#                        __long_description__))
 __setup_requires__ = ['pytest-runner', ]
 __tests_require__ = ['pytest', 'pytest-cov', 'pytest-pep8']
--- setup.py
+++ setup.py
@@ -12,6 +12,10 @@ infopath = os.path.abspath(os.path.join(os.path.dirname(__file__),
 with open(infopath) as open_file:
     exec(open_file.read(), release_info)
 
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    __about__ = f.read()
+
 setup(
     name=__name__,
     author=release_info['__author__'],
@@ -22,7 +26,8 @@ setup(
     install_requires=release_info['__requires__'],
     license=release_info['__license__'],
     description=release_info['__description__'],
-    long_description=release_info['__about__'],
+    long_description=__about__,
+    long_description_content_type='text/markdown',
     setup_requires=release_info['__setup_requires__'],
     tests_require=release_info['__tests_require__']
 )
